2fc8f75a6659ed6fcf1b2d39be14ea2bd6587e53
Diego Queiros <diegoqueiros@hotmail.com>
2023-02-10 16:33:51 -0300

    Métodos de Extensão

---
+++ CursoCSharp/CursoCSharp/MetodosdeExtensao/Client.cs
@@ -0,0 +1,17 @@
+﻿
+using CursoCSharp.MetodosdeExtensao.Utils;
+
+namespace CursoCSharp.MetodosdeExtensao
+{
+    public class Client
+    {
+        public void TestaClasse()
+        {
+            var txt1 = "curso";
+            var txt2 = "disciplina";
+
+            txt1 = txt1.CaixaAltaPrimeiraLetra();
+            txt2 = txt2.CaixaAltaPrimeiraLetra();
+        }
+    }
+}
---
+++ CursoCSharp/CursoCSharp/MetodosdeExtensao/Utils/MetodosExtensao.cs
@@ -0,0 +1,18 @@
+﻿
+namespace CursoCSharp.MetodosdeExtensao.Utils
+{
+    public static class MetodosExtensao
+    {
+        public static string CaixaAltaPrimeiraLetra(this string value)
+        {
+            if (value.Length > 0)
+            {
+                char[] array = value.ToCharArray();
+                array[0] = char.ToUpper(array[0]);
+                return new string(array);
+            }
+
+            return value;
+        }
+    }
+}
