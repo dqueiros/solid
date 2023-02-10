579c414467447b16e3e18f7131f5a716a1a0abfd
Diego Queiros <diegoqueiros@hotmail.com>
2023-02-10 15:45:29 -0300

    Parâmetros Opcionais

---
+++ CursoCSharp/CursoCSharp/ParametrosOpcionais/Calcular.cs
@@ -0,0 +1,18 @@
+﻿
+using System.Reflection.Metadata.Ecma335;
+
+namespace CursoCSharp.ParametrosOpcionais
+{
+    public class Calcular
+    {
+        //Parâmetro normal: x
+        //Parâmetro Opcional: y, z, w
+            //Valor padrão e constante
+            //Os parâmetros opcionais deve ser definido após os parametros normais, ou seja,
+            //eles são sempre os ultimos parâmetros do método.
+        public int Somar(int x, int y = 20, int z = 30, int w = 50)
+        {
+            return x + y + z + w;
+        }
+    }
+}
