cd799efcaf378f89e9563e91e194444694e9a6e1
Diego Queiros <diegoqueiros@hotmail.com>
2023-02-10 15:25:38 -0300

    Métodos e Sobrecarga de Métodos

---
+++ CursoCSharp/CursoCSharp/MetodoseSobrecargadeMetodos/Calculo.cs
@@ -0,0 +1,31 @@
+﻿
+namespace CursoCSharp.MetodoseSobrecargadeMetodos
+{
+    public class Calculo
+    {
+        public int Somar(int num1, int num2) 
+        {
+            return num1 + num2;
+        }
+
+        public double Somar(double num1, int num2)
+        {
+            return num1 + num2;
+        }
+
+        public int Somar(int num1, int num2, int num3)
+        {
+            return num1 + num2 + num3;
+        }
+
+        public int Somar(params int[] nums)
+        {
+            var resultado = 0;
+            foreach (var num in nums)
+                resultado += num;
+
+            return resultado;
+
+        }
+    }
+}
