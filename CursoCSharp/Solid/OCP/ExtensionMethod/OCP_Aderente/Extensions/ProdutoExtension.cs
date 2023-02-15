e060c7a5f3692c75215575231443fcbf3034ecea
Diego Queiros <diegoqueiros@hotmail.com>
2023-02-15 08:12:21 -0300

    Organização em pasta

---
+++ CursoCSharp/Solid/OCP/ExtensionMethod/OCP_Aderente/Extensions/ProdutoExtension.cs
@@ -0,0 +1,20 @@
+﻿namespace Solid.OCP.ExtensionMethod.OCP_Aderente.Extensions
+{
+    public static class ProdutoExtension
+    {
+        //OCP - Usando Métodos de Extensão
+        public static double MargemLucro(this Produto produto)
+        {
+            try
+            {
+                var lucroLiquido = produto.Venda - produto.Custo;
+                var margemLucro = lucroLiquido / produto.Venda * 100;
+                return Convert.ToDouble(margemLucro);
+            }
+            catch (Exception)
+            {
+                throw new InvalidOperationException("Erro ao calcular a margem de lucro");
+            }
+        }
+    }
+}
