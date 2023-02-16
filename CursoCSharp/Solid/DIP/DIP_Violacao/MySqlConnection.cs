434589879a3f6a85b85c32d4faa6d40cddf8430e
Diego Queiros <diegoqueiros@hotmail.com>
2023-02-15 21:48:09 -0300

    DIP - Violacao

---
+++ CursoCSharp/Solid/DIP/DIP_Violacao/MySqlConnection.cs
@@ -0,0 +1,11 @@
+﻿
+namespace Solid.DIP.DIP_Violacao
+{
+    public class MySqlConnection
+    {
+        public void Conectar()
+        {
+            Console.WriteLine("Conexão com MySQL");
+        }
+    }
+}
---
+++ CursoCSharp/Solid/DIP/DIP_Violacao/RecuperarSenha.cs
@@ -0,0 +1,14 @@
+﻿
+namespace Solid.DIP.DIP_Violacao
+{
+    public class RecuperarSenha
+    {
+        private MySqlConnection _conexaoBanco;
+
+        public RecuperarSenha()
+        {
+            _conexaoBanco = new MySqlConnection();
+        }
+        //codigo para realizar operações
+    }
+}
