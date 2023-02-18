56c0ae92f01bae9471c4fb725be5f68f48644294
dqueiros <diegoqueiros@hotmail.com>
2023-02-18 10:55:03 -0300

    Corrigindo os números mágicos - Enum e switch

--- CursoCSharp/ProjetoPratico/CodeClean/GerenciadorDeDescontos.cs
+++ CursoCSharp/ProjetoPratico/CodeClean/GerenciadorDeDescontos.cs
@@ -1,34 +1,33 @@
 ﻿
 namespace ProjetoPratico.CodeClean;
 
-public class GerenciadorDeDescontos
+public partial class GerenciadorDeDescontos
 {
-    public decimal AplicarDesconto(decimal precoProduto, int statusContaCliente, int tempoDeContasEmAnos)
+    public decimal AplicarDesconto(decimal precoProduto, StatusContaCliente statusContaCliente, int tempoDeContasEmAnos)
     {
         decimal precoAposDesconto = 0;
 
         decimal descontoPorFidelidade = (tempoDeContasEmAnos > 5) ? 
             (decimal)5 / 100 : 
             (decimal)tempoDeContasEmAnos / 100;
-        
-        if (statusContaCliente == 1)
+
+        switch (statusContaCliente)
         {
-            precoAposDesconto = precoProduto;
-        }
-        else if (statusContaCliente == 2)
-        {
-            precoAposDesconto = (precoProduto - (0.1m * precoProduto)) - 
-                descontoPorFidelidade * (precoProduto - (0.1m * precoProduto));
-        }
-        else if (statusContaCliente == 3)
-        {
-            precoAposDesconto = (0.7m * precoProduto) - 
-                descontoPorFidelidade * (0.7m * precoProduto);
-        }
-        else if (statusContaCliente == 4)
-        {
-            precoAposDesconto = (precoProduto - (0.5m * precoProduto)) 
-                - descontoPorFidelidade * (precoProduto - (0.5m * precoProduto));
+            case StatusContaCliente.NaoRegistrado:
+                precoAposDesconto = precoProduto;
+                break;
+            case StatusContaCliente.ClienteComum:
+                precoAposDesconto = (precoProduto - (0.1m * precoProduto)) - 
+                    descontoPorFidelidade * (precoProduto - (0.1m * precoProduto));
+                break;
+            case StatusContaCliente.ClienteEspecial:
+                precoAposDesconto = (0.7m * precoProduto) -
+                    descontoPorFidelidade * (0.7m * precoProduto);
+                break;
+            case StatusContaCliente.ClienteVIP:
+                precoAposDesconto = (precoProduto - (0.5m * precoProduto))
+                    - descontoPorFidelidade * (precoProduto - (0.5m * precoProduto));
+                break;
         }
         return precoAposDesconto;
     }
---
+++ CursoCSharp/ProjetoPratico/CodeClean/StatusContaCliente.cs
@@ -0,0 +1,13 @@
+﻿
+namespace ProjetoPratico.CodeClean;
+
+public partial class GerenciadorDeDescontos
+{
+    public enum StatusContaCliente
+    {
+        NaoRegistrado = 1,
+        ClienteComum = 2,
+        ClienteEspecial = 3,
+        ClienteVIP = 4
+    }
+}
