975d0767719c16e27320bc71cd3a68427bb9057f
dqueiros <diegoqueiros@hotmail.com>
2023-02-18 10:40:55 -0300

    Organizando o código - Ajustando a Nomenclatura com PascalCase e CamelCase

---
+++ CursoCSharp/ProjetoPratico/CodeClean/GerenciadorDeDescontos.cs
@@ -0,0 +1,35 @@
+﻿
+namespace ProjetoPratico.CodeClean;
+
+public class GerenciadorDeDescontos
+{
+    public decimal AplicarDesconto(decimal precoProduto, int statusContaCliente, int tempoDeContasEmAnos)
+    {
+        decimal precoAposDesconto = 0;
+
+        decimal descontoPorFidelidade = (tempoDeContasEmAnos > 5) ? 
+            (decimal)5 / 100 : 
+            (decimal)tempoDeContasEmAnos / 100;
+        
+        if (statusContaCliente == 1)
+        {
+            precoAposDesconto = precoProduto;
+        }
+        else if (statusContaCliente == 2)
+        {
+            precoAposDesconto = (precoProduto - (0.1m * precoProduto)) - 
+                descontoPorFidelidade * (precoProduto - (0.1m * precoProduto));
+        }
+        else if (statusContaCliente == 3)
+        {
+            precoAposDesconto = (0.7m * precoProduto) - 
+                descontoPorFidelidade * (0.7m * precoProduto);
+        }
+        else if (statusContaCliente == 4)
+        {
+            precoAposDesconto = (precoProduto - (0.5m * precoProduto)) 
+                - descontoPorFidelidade * (precoProduto - (0.5m * precoProduto));
+        }
+        return precoAposDesconto;
+    }
+}
